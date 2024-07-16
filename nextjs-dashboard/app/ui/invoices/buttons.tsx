afc22b1855b2efe782a49803b19db6873c9a92f9
siobhan <sr@saibhreas.com>
2024-07-15 20:23:59 -0400

    CRUD complete ontoe 13

--- nextjs-dashboard/app/ui/invoices/buttons.tsx
+++ nextjs-dashboard/app/ui/invoices/buttons.tsx
@@ -1,6 +1,8 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
+import { deleteInvoice } from '@/app/lib/actions';
+
 //CREATE
 export function CreateInvoice() {
   return (
@@ -28,12 +30,13 @@ export function UpdateInvoice({ id }: { id: string }) {
 
 //DELETE
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
-    <>
+    <form action={deleteInvoiceWithId}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
-    </>
+    </form>
   );
 }
